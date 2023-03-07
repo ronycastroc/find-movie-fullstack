ffe7cc275edb5362cfffb57e32af9815988bda76
ronycastroc <castroroony@gmail.com>
2023-03-07 15:09:19 -0300

    feat: rendering full movie

--- find-movie-front/src/components/Movie/Movie.js
+++ find-movie-front/src/components/Movie/Movie.js
@@ -1,28 +1,38 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Stars, Button } from '../../components';
 import './movie.scss';
 
 export const Movie = () => {
   const movie = useSelector(state => state.movie);
-  console.log(movie);
+
+  useEffect(() => {
+    if (movie?.Response === "False") return alert("Movie not found!");
+  }, [movie]); 
 
   return (
-    <div className='movie'>
-      <div className='info'>
-        <h1>{movie?.Title}</h1>
-        <p>{movie?.Plot}</p>
-        <span className='actors'>Actors </span>
-        <span>{movie?.Actors}</span>
-        <div className='star'>
-          <p className='review'>Review </p>
-          <Stars />
+    <>
+      {!movie || movie?.Response === "False" ? ("") : 
+      (<>
+        <div className='movie'>
+          <div className='info'>
+            <h1>{movie?.Title}</h1>
+            <p>{movie?.Plot}</p>
+            <span className='actors'>Actors </span>
+            <span>{movie?.Actors}</span>
+            <div className='star'>
+              <p className='review'>Review </p>
+              <Stars />
+            </div>
+
+            <Button>Favorite</Button>
+          </div>
+          <div className='poster'>
+            <img src={movie?.Poster} alt="" />
+
+          </div>
         </div>
-
-        <Button>Favorite</Button>
-      </div>
-      <div>
-
-      </div>
-    </div>
+      </>)}
+    </>
   );
 }
