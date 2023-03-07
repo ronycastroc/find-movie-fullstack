129c66dbea7e489926803ebb19acaa9fb4f8a878
ronycastroc <castroroony@gmail.com>
2023-03-07 06:12:55 -0300

    initial config front

---
+++ find-movie-front/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './App';
+
+import './assets/styles/reset.scss';
+import './assets/styles/style.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
